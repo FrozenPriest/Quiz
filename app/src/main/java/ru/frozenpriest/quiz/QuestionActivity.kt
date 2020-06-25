3706cc1e410d4678bea214390db2d70d85bb3f49
FrozenPriest <frozenpriest@mail.ru>
2020-06-25 21:17:17 +0300

    first ver

---
+++ app/src/main/java/ru/frozenpriest/quiz/QuestionActivity.kt
@@ -0,0 +1,16 @@
+package ru.frozenpriest.quiz
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+
+    fun answerQuestion(view: View) {
+
+    }
+}
